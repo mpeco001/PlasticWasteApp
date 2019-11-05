571d8b8611cce3eb5c1bf6466991e4e980afbb5a
Mariano Pecorella <33729721+mpeco001@users.noreply.github.com>
2019-11-06 00:07:48 +0100

    Add files via upload
    commit of the project first skeleton

---
+++ PlasticApp/PlasticApp/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  PlasticApp
+//
+//  Created by Mariano on 05/11/2019.
+//  Copyright © 2019 Mariano. All rightsvar <#variable name#>: <#type#> {
+    get {
+        <#statements#>
+    }
+    set {
+        <#variable name#> = newValue
+    }
+} reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
